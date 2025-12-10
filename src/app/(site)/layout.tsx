3ad6fba8f32a1f3aecbe406ff4e0a9fa3b456101
Remjest <remjest@mail.ru>
2025-12-10 14:10:36 +0300

    fix ssr metrika

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -1,4 +1,3 @@
-import Script from 'next/script';
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "../globals.css";
@@ -15,6 +14,7 @@ import Phone from './img/phone.png';
 import WhatsApp from './img/whatsapp.png';
 import Tg from './img/tg.png';
 import Max from './img/max.png';
+import YandexMetrica from "./yandexMetrica";
 
 
 const montserrat = Montserrat({
@@ -58,36 +58,7 @@ export default function MainLayout({ children }: MainLayoutProps) {
     return (
         <html lang="ru">
             <body className={classNames(montserrat.variable, styles.layout)}>
-                <Script
-                id="yandex-metrica"
-                strategy="afterInteractive"
-                dangerouslySetInnerHTML={{
-                    __html: `
-                    (function(m,e,t,r,i,k,a){
-                        m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-                        m[i].l=1*new Date();
-                        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-                        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-                    })(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js', 'ym');
-
-                    ym(83079244, 'init', {
-                        webvisor: true,
-                        clickmap: true,
-                        accurateTrackBounce: true,
-                        trackLinks: true
-                    });
-                    `,
-                }}
-                />
-                <noscript>
-                <div>
-                    <img
-                    src="https://mc.yandex.ru/watch/83079244"
-                    style={{ position: 'absolute', left: '-9999px' }}
-                    alt=""
-                    />
-                </div>
-                </noscript>
+                <YandexMetrica />
                 <Header menu='client' className={styles.header} />
                 <Sidebar menu='client' className={styles.sidebar} />
                 <Main className={styles.main}>
